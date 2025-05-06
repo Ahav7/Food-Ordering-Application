6fe195639700b60ecf3e21a4ee8a3180d3618c35
Hemanth <hemanthkrishnasai1@gmail.com>
2025-05-06 12:19:54 -0500

    Fix admin issues

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,12 +1,12 @@
 import bcrypt from "bcrypt";
-import {UserInfo} from "@/models/UserInfo";
+import { UserInfo } from "@/models/UserInfo";
 import clientPromise from "@/libs/mongoConnect";
 import * as mongoose from "mongoose";
-import {User} from '@/models/User';
-import NextAuth, {getServerSession} from "next-auth";
+import { User } from '@/models/User';
+import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 
 export const authOptions = {
   secret: process.env.SECRET,
@@ -23,37 +23,19 @@ export const authOptions = {
         username: { label: "Email", type: "email", placeholder: "test@example.com" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials, req) {
+      async authorize(credentials) {
         const email = credentials?.email;
         const password = credentials?.password;
 
-        mongoose.connect(process.env.MONGO_URL);
-        const user = await User.findOne({email});
+        await mongoose.connect(process.env.MONGO_URL);
+        const user = await User.findOne({ email });
         const passwordOk = user && bcrypt.compareSync(password, user.password);
 
-        if (passwordOk) {
-          return user;
-        }
-
-        return null
+        return passwordOk ? user : null;
       }
     })
   ],
 };
 
-export async function isAdmin() {
-  const session = await getServerSession(authOptions);
-  const userEmail = session?.user?.email;
-  if (!userEmail) {
-    return false;
-  }
-  const userInfo = await UserInfo.findOne({email:userEmail});
-  if (!userInfo) {
-    return false;
-  }
-  return userInfo.admin;
-}
-
 const handler = NextAuth(authOptions);
-
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST };
